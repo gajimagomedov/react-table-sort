9ac178d0a0df586e16fb6c923d44dc1b0d889a0a
gadjimagomedov <islam7581@mail.ru>
2017-11-01 00:15:33 +0300

    принимает свойство user

--- src/components/UserData.js
+++ src/components/UserData.js
@@ -3,16 +3,17 @@ import {PropTypes} from 'prop-types';
 
 
 function UserData({update, user, index}){
-    
+
     return(
         <tr onClick={() => update({active: index})}>
-            <td>{props.id}</td>
-            <td>{props.name}</td>
-            <td>{props.lastName}</td>
-            <td>{props.email}</td>
-            <td>{props.phone}</td>
+            <td>{user.id}</td>
+            <td>{user.firstName}</td>
+            <td>{user.lastName}</td>
+            <td>{user.email}</td>
+            <td>{user.phone}</td>
         </tr>
     );
-}
+    
+};
 
 export default UserData;
