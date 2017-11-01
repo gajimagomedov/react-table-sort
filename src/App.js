52e11a580f75076d1da623a86874f758d48d4be0
gadjimagomedov <islam7581@mail.ru>
2017-11-01 23:42:49 +0300

    добавил три компоненты в метод render: SearchBar,ActiveUser,UserList

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import {PropTypes} from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
-import load from './utils/load';
+import UserList from './components/UserList';
+import ActiveUser from './components/ActiveUser';
+import SearchBar from './components/SearchBar';
+import Preloader from './components/Preloader';
 
 class App extends Component {
 
@@ -14,50 +17,46 @@ class App extends Component {
       active: 0,
       term:''
     }
-
-    this.loadData();
     
+    this.updateData = this.updateData.bind(this);
+  }
+
+  componentDidMount(){
+    fetch(this.props.data)
+      .then(res => res.json())
+      .then(data => { this.initialData = data;  this.setState({ data: this.initialData });})   
+      .catch(error => console.log(error));   
   }
 
 
-  loadData(){
-    load(this.props.data).then(res => {
-      // this.setState({
-      //   data: JSON.parse(res)
-      // });
-      console.log(JSON.parse(res));
-    });
+  updateData(config){
+    this.setState(config)
   }
 
-
-
-
-  render() {
+  renderDisplay(){
     return (
       <div className="App">
-
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Table-Data</h1>
-        </header>
-
-        <main>
-
-
-
-
-        </main>
-     
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Table-Data</h1>
+        <SearchBar update={this.updateData} data={this.initialData} term={this.state.term} />
+      </header>
+      <main className="wrapper">
+        <div className="user-active">
+          <ActiveUser data={this.state.data} active={this.state.active} /> 
+        </div>
+        <div className="user-list">    
+          <UserList data={this.state.data} update={this.updateData}/> 
+        </div> 
+      </main>
+    </div>
     );
   }
 
+  render() {
+     return this.state.data ? this.renderDisplay() : <Preloader />
+  }
 
-
-}
-
-App.propTypes = {
-  data: PropTypes.string
 }
 
 export default App;
