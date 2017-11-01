fb49a95df1250f401f8af5a3865c78a3ddce8478
gadjimagomedov <islam7581@mail.ru>
2017-11-01 12:51:35 +0300

    добавлена компонента SearchBar, для поиска пользователя

---
+++ src/components/SearchBar.js
@@ -0,0 +1,39 @@
+import React, {Component} from "react";
+
+
+function SearchBar({term, data, update}) {
+    console.log(data);
+
+    const dataSearch = e => {
+        const value = e.target.value.toLowerCase();
+
+        const filter = data.filter(user =>{
+            return user.firstName.toLowerCase().includes(value);
+        });
+
+        update({
+            data: filter,
+            active: 0,
+            term: value
+        })
+
+    };
+
+    return (
+        <div className="search-bar">
+            <input
+                value={term}
+                type="text"
+                className="search-bar__input"
+                placeholder="Поиск..."
+                onChange={dataSearch}
+            />
+        </div>
+    );
+
+
+
+}
+
+
+export default SearchBar;
