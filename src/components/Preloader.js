91edca155d837f263e82322aa9a7a245f989d85f
gadjimagomedov <islam7581@mail.ru>
2017-11-01 15:51:39 +0300

    компонент Preloader, появляется при загрузке страницы

---
+++ src/components/Preloader.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Preloader(props){
+    return (
+        <div className="wrapper-preload">
+            <div className="circle"></div>
+        </div>
+    );
+}
+
+export default Preloader;
